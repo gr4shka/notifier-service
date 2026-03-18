2946a15164db6cf67f42835ebdddd238108e0f93
grush <girois19@gmail.com>
2026-03-18 22:04:54 +0500

    implement Notifier struct with Send, Close, NewNotifier

---
+++ notifier.go
@@ -0,0 +1,55 @@
+package notifierservice
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type Message struct {
+	ID      string
+	Payload string
+}
+
+type ExternalClient interface {
+	Post(ctx context.Context, msg Message) (statusCode int, err error)
+}
+
+type Notifier struct {
+	client  ExternalClient
+	jobs    chan Message
+	workers int
+	limiter *time.Ticker
+	wg      sync.WaitGroup
+	closed  atomic.Bool
+	stats   Stats
+}
+
+func NewNotifier(client ExternalClient, worker int, rate int) *Notifier {
+	n := &Notifier{
+		client:  client,
+		jobs:    make(chan Message, 1000),
+		workers: worker,
+		limiter: time.NewTicker(time.Second / time.Duration(rate)),
+	}
+
+	return n
+}
+
+func (n *Notifier) Send(msg Message) error {
+	if n.closed.Load() {
+		return errors.New("notifier closed")
+	}
+
+	n.jobs <- msg
+	return nil
+}
+
+func (n *Notifier) Close() {
+	n.closed.Store(true)
+	close(n.jobs)
+	n.wg.Wait()
+	n.limiter.Stop()
+}
