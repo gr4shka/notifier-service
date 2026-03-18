982e04604bcc44b738cd543744a50d20e105a13e
grush <girois19@gmail.com>
2026-03-18 22:38:53 +0500

    add mutex control in Send

--- notifier.go
+++ notifier.go
@@ -44,6 +44,9 @@ func NewNotifier(client ExternalClient, worker int, rate int) *Notifier {
 }
 
 func (n *Notifier) Send(ctx context.Context, msg Message) error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
 	if n.closed.Load() {
 		return errors.New("notifier closed")
 	}
